e1c89bdfec480f8204c90f24518af31d4e92e66b
Collin-Varisco <70452699+Collin-Varisco@users.noreply.github.com>
2020-11-15 20:43:57 -0600

    second update

---
+++ Assets/Scripts/Count.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count : MonoBehaviour
+{
+    private int PLAYER_HEALTH = 100;
+    public Text countText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countText.text = "HEALTH: " + PLAYER_HEALTH.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fruit"))
+        {
+            PLAYER_HEALTH -= 25;
+            countText.text = "Health: " + PLAYER_HEALTH.ToString();
+        }
+    }
+}
